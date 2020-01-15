764863bd13d5a0d5df9291ce5f7832c2a9fd6298
Philip Akpeki <philipakpeki@gmail.com>
2020-01-15 11:16:58 +0100

    completed the code for the angular-reddit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ArticleComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
